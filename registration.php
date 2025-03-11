ce2ae51f08c6ce4b85b3b5284dfa04adc957182d
Joseph Leedy <joseph.leedy@boldcommerce.com>
2025-03-11 11:11:52 -0600

    Fix incorrect casing in extension name (CHK-4747)

--- registration.php
+++ registration.php
@@ -4,6 +4,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Bold_BoosterOnestepcheckoutCompatibility',
+    'Bold_BoosterOneStepCheckoutCompatibility',
     __DIR__
 );
--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,10 +2,10 @@ let config = {
     config: {
         mixins: {
             'Onestepcheckout_Iosc/js/view/billing-address': {
-                'Bold_BoosterOnestepcheckoutCompatibility/js/view/billing-address': true
+                'Bold_BoosterOneStepCheckoutCompatibility/js/view/billing-address': true
             },
             'Onestepcheckout_Iosc/js/registration': {
-                'Bold_BoosterOnestepcheckoutCompatibility/js/registration-mixin': true
+                'Bold_BoosterOneStepCheckoutCompatibility/js/registration-mixin': true
             }
         }
     }

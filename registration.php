9bfca336c94cd22244e0a61cac8a6e7bb7553d63
Joseph Leedy <joseph.leedy@boldcommerce.com>
2025-03-11 11:05:32 -0600

    Fix incorrect extension name (CHK-4747)

--- registration.php
+++ registration.php
@@ -4,6 +4,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'Bold_OnestepcheckoutIoscCompatibility',
+    'Bold_BoosterOnestepcheckoutCompatibility',
     __DIR__
 );
--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,10 +2,10 @@ let config = {
     config: {
         mixins: {
             'Onestepcheckout_Iosc/js/view/billing-address': {
-                'Bold_OnestepcheckoutIoscCompatibility/js/view/billing-address': true
+                'Bold_BoosterOnestepcheckoutCompatibility/js/view/billing-address': true
             },
             'Onestepcheckout_Iosc/js/registration': {
-                'Bold_OnestepcheckoutIoscCompatibility/js/registration-mixin': true
+                'Bold_BoosterOnestepcheckoutCompatibility/js/registration-mixin': true
             }
         }
     }
